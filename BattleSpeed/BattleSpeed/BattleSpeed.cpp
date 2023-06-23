730fb7e9a3f91f7a0cbd4401308309adaf415660
igrikNmG <igrik1987@gmail.com>
2023-06-23 09:11:49 +0300

    Исправлен вылет при нажатии кнопки по дефолту

--- BattleSpeed/BattleSpeed/BattleSpeed.cpp
+++ BattleSpeed/BattleSpeed/BattleSpeed.cpp
@@ -4,7 +4,8 @@ Patcher* _P;
 PatcherInstance* _PI;
 
 #define BattleSpeed (*(_int_*)0x69883C)
-#define BATTON_ID 240
+#define BUTTON_ID 240
+#define DEFAULT_BUTTON_ID 201
 
 // константы боевых скоростей
 // const float battleSpeedCoef[10] = { 1.00f, 0.61f, 0.43f, 0.21f, 0.090f, 0.075f, 0.060f, 0.045f, 0.030f, 0.010f };
@@ -26,30 +27,51 @@ int __stdcall Y_Dlg_BattleOptions_Proc(HiHook* hook, _EventMsg_* msg)
 
 	if (msg->type == MT_MOUSEBUTTON)
 	{
+        // ПКМ по элементу диалога 
 		if (msg->subtype == MST_RBUTTONDOWN)
 		{
-			if (msg->item_id >= BATTON_ID && msg->item_id <= (BATTON_ID +9) )
+			if (msg->item_id >= BUTTON_ID && msg->item_id <= (BUTTON_ID +9) )
 			{
 				// описания скорости кнопок (подгоняем под стандарные 3 описания)
-				msg->item_id = ( (msg->item_id - BATTON_ID -1) / 3 +230 );
+				msg->item_id = ( (msg->item_id - BUTTON_ID -1) / 3 +230 );
 			}
 		}
+
+        // ЛКМ по элементу диалога 
 		if (msg->subtype == MST_LBUTTONDOWN)
 		{
 			int ID = msg->item_id;
-			if ( msg->item_id >= BATTON_ID && msg->item_id <= (BATTON_ID +9) )
+			if ( ID >= BUTTON_ID && ID <= (BUTTON_ID +9) )
 			{
 				for (int i = 0; i < 10; i++)
 				{
-					((_DlgStaticDef_*)dlg->GetItem(BATTON_ID + i))->Hide_ButStayEnable();
+					((_DlgStaticDef_*)dlg->GetItem(BUTTON_ID + i))->Hide_ButStayEnable();
 				}
-				BattleSpeed = ID - BATTON_ID;
-				((_DlgStaticDef_*)dlg->GetItem(BATTON_ID + BattleSpeed))->Show_ButStayEnable();
+				BattleSpeed = ID - BUTTON_ID;
+				((_DlgStaticDef_*)dlg->GetItem(BUTTON_ID + BattleSpeed))->Show_ButStayEnable();
 
 				dlg->Field<byte>(0x4C) = 1; // в диалоге нужно сохранить параметры
 				dlg->Redraw();
 			}
 		}
+
+        // ЛКМ по кнопке диалога 
+        if (msg->subtype == MST_LBUTTONCLICK)
+        {
+            // default button
+            if (msg->item_id == DEFAULT_BUTTON_ID) {
+				for (int i = 0; i < 10; i++)
+				{
+					((_DlgStaticDef_*)dlg->GetItem(BUTTON_ID + i))->Hide_ButStayEnable();
+				}
+				BattleSpeed = 1;
+				((_DlgStaticDef_*)dlg->GetItem(BUTTON_ID +1))->Show_ButStayEnable();
+
+				dlg->Field<byte>(0x4C) = 1;
+				dlg->Redraw();
+            }
+
+        }
 	}
 
 	return CALL_1(int, __thiscall, hook->GetDefaultFunc(), msg);
@@ -63,10 +85,10 @@ _Dlg_* __stdcall Y_Dlg_BattleOptions_Create(HiHook* hook, _Dlg_* dlg)
 
 	for (int i = 0; i < 10; i++)
 	{
-		dlg->AddItem(_DlgStaticDef_::Create(29 + 19*i, 225, 18, 36, BATTON_ID +i, "SYSLB.DEF", i, 0, 0));
-		((_DlgStaticDef_*)dlg->GetItem(BATTON_ID + i))->Hide_ButStayEnable();  // сразу же скрываем кнопки (но они работают)
+		dlg->AddItem(_DlgStaticDef_::Create(29 + 19*i, 225, 18, 36, BUTTON_ID +i, "SYSLB.DEF", i, 0, 0));
+		((_DlgStaticDef_*)dlg->GetItem(BUTTON_ID + i))->Hide_ButStayEnable();  // сразу же скрываем кнопки (но они работают)
 	}
-	((_DlgStaticDef_*)dlg->GetItem(BATTON_ID + BattleSpeed))->Show_ButStayEnable();
+	((_DlgStaticDef_*)dlg->GetItem(BUTTON_ID + BattleSpeed))->Show_ButStayEnable();
 
 	return dlg;	
 }
@@ -99,6 +121,12 @@ void StartPlugin()
 	// пропускаем создание кнопок в диалоге системных опций битвы
 	_PI->WriteHexPatch(0x46E1F0, "E9 F7000000"); // JMP 0046E2EC
 	_PI->WriteHexPatch(0x46F07A, "EB 44"); // JMP SHORT 0046F0C0
+
+
+    // default button
+    _PI->WriteDword(0x46F41D +1, BUTTON_ID);
+    _PI->WriteDword(0x46F3EF +1, BUTTON_ID);
+    _PI->WriteDword(0x46F40B +2, BUTTON_ID +10);
 
 	// добавление новых кнопок в диалог системных опций
 	_PI->WriteHiHook(0x46DF00, SPLICE_, EXTENDED_, THISCALL_, Y_Dlg_BattleOptions_Create);
