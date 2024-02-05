593de26329faf3c03a7cbc4524e7e467e1a258fe
igrik <igrik1987@gmail.com>
2024-02-05 20:03:06 +0300

    fixed tubs to spases

--- BattleReplay/BattleReplay/BattleReplay.cpp
+++ BattleReplay/BattleReplay/BattleReplay.cpp
@@ -1,5 +1,4 @@
 #include "..\..\..\include\homm3.h"
-
 #include "..\..\..\include\era.h"
 using namespace Era;
 
@@ -32,11 +31,11 @@ _bool_ isNeedReplay, ifCanReplay;
 
 int __stdcall Y_SkipAddExp(LoHook* h, HookContext* c)
 {  // 0x477254 (первый выполняемый хук)
-	if (isNeedReplay) { 
-		c->return_address = 0x477303;
-		return NO_EXEC_DEFAULT;
-	}
-	return EXEC_DEFAULT; 
+    if (isNeedReplay) { 
+        c->return_address = 0x477303;
+        return NO_EXEC_DEFAULT;
+    }
+    return EXEC_DEFAULT; 
 }
 
 int __stdcall Y_SkipRedrawAdvMap(LoHook* h, HookContext* c)
@@ -63,75 +62,75 @@ int __stdcall Y_AfterBattle_SkipAll(LoHook* h, HookContext* c)
 
 _Dlg_* __stdcall Y_Dlg_BattleResults_Create(HiHook* hook, _Dlg_* dlg, _Hero_* heroA, _Hero_* heroD, _int_ dlgDef, _int_ sideWin, _int_ a6, _int_ allHPLoserLose)
 {
-	CALL_7(_Dlg_*, __thiscall, hook->GetDefaultFunc(), dlg, heroA, heroD, dlgDef, sideWin, a6, allHPLoserLose);
+    CALL_7(_Dlg_*, __thiscall, hook->GetDefaultFunc(), dlg, heroA, heroD, dlgDef, sideWin, a6, allHPLoserLose);
 
-	if (ifCanReplay) {
-		dlg->AddItem(_DlgStaticPcx8_::Create(20, 506, 0, "Box64x30.pcx"));
-		dlg->AddItem(_DlgButton_::Create(21, 507, 64, 30, DLG_CANCEL, "iCancel.def", 0, 1, 0, 1, 2)); 
-	}
-	return dlg;
+    if (ifCanReplay) {
+        dlg->AddItem(_DlgStaticPcx8_::Create(20, 506, 0, "Box64x30.pcx"));
+        dlg->AddItem(_DlgButton_::Create(21, 507, 64, 30, DLG_CANCEL, "iCancel.def", 0, 1, 0, 1, 2)); 
+    }
+    return dlg;
 }
 
 int __stdcall Y_Dlg_BattleResults_Proc(HiHook* hook, _EventMsg_* msg)
 {
-	if (ifCanReplay) {
-		isNeedReplay = false;
-		if (msg->type == MT_MOUSEBUTTON && msg->type != MT_KEYDOWN) {
-			if (msg->subtype == MST_LBUTTONCLICK) {
-				if (msg->item_id == DLG_CANCEL) {
+    if (ifCanReplay) {
+        isNeedReplay = false;
+        if (msg->type == MT_MOUSEBUTTON && msg->type != MT_KEYDOWN) {
+            if (msg->subtype == MST_LBUTTONCLICK) {
+                if (msg->item_id == DLG_CANCEL) {
                     o_WndMgr->result_dlg_item_id == DLG_CANCEL;
                     isNeedReplay = true;
 
-					o_TimeClick = 0; 					
+                    o_TimeClick = 0;
                     msg->type = 512;
                     msg->subtype = 10;
-                    msg->item_id = 10;					
+                    msg->item_id = 10;
 
                     return 2;
-				}
-			}
-		}
-	}
-	return CALL_1(int, __thiscall, hook->GetDefaultFunc(), msg);
+                }
+            }
+        }
+    }
+    return CALL_1(int, __thiscall, hook->GetDefaultFunc(), msg);
 }
 
 ////////////////////////////////////////////////////////////////////////// 
 ////////////////////////////////////////////////////////////////////////// 
 
 int __stdcall Y_ReplayBattle(HiHook* hook, _AdvMgr_* advMng, _dword_ MixedPos, _Hero_* HrA, _Army_* MArrA, _int_ OwnerD, _dword_ townD, _Hero_* HrD, _Army_* MArrD, _int_ Pv3, _dword_ Pv2, _dword_ Pv1)
-{	
-	int ret = 0; 
-	ifCanReplay = false;   
+{   
+    int ret = 0; 
+    ifCanReplay = false;
 
-	_GameMgr_* gm = o_GameMgr;
-	char isAHuman = 0;
-	char isDHuman = 0;
-	if (HrA) { 
-		isAHuman = gm->GetPlayer(HrA->owner_id)->IsHuman(); 
-	}
+    _GameMgr_* gm = o_GameMgr;
+    char isAHuman = 0;
+    char isDHuman = 0;
+    if (HrA) { 
+        isAHuman = gm->GetPlayer(HrA->owner_id)->IsHuman(); 
+    }
 
-	if (OwnerD >= 0 ) {	
-		isDHuman = gm->GetPlayer(OwnerD)->IsHuman(); 
-	}
+    if (OwnerD >= 0 ) {
+        isDHuman = gm->GetPlayer(OwnerD)->IsHuman(); 
+    }
 
-	if ( o_GameMgrType == 2 && isAHuman && isDHuman ) { 
-		// b_MsgBox("Переигрывание битвы ОТКЛЮЧЕНО", 1);
-		return CALL_11(int, __thiscall, hook->GetDefaultFunc(), advMng, MixedPos, HrA, MArrA, OwnerD, townD, HrD, MArrD, Pv3, Pv2, Pv1);
-	} 
+    if ( o_GameMgrType == 2 && isAHuman && isDHuman ) { 
+        // b_MsgBox("Переигрывание битвы ОТКЛЮЧЕНО", 1);
+        return CALL_11(int, __thiscall, hook->GetDefaultFunc(), advMng, MixedPos, HrA, MArrA, OwnerD, townD, HrD, MArrD, Pv3, Pv2, Pv1);
+    } 
 
-	ifCanReplay = true;
-	_int_ saveParamQB = o_QuickBattle;
+    ifCanReplay = true;
+    _int_ saveParamQB = o_QuickBattle;
 
-	_Hero_* HrAS = 0;
-	_Hero_* HrDS = 0;
-	_Npc_* npcA = 0;
-	_Npc_* npcAS = 0;
-	_bool_ isNpcASCreate = false;
-	_bool_ isNpcDSCreate = false;
-	_Npc_* npcD = 0;
-	_Npc_* npcDS = 0;
-	_Army_* armyAS = 0;
-	_Army_* armyDS = 0;    
+    _Hero_* HrAS = 0;
+    _Hero_* HrDS = 0;
+    _Npc_* npcA = 0;
+    _Npc_* npcAS = 0;
+    _bool_ isNpcASCreate = false;
+    _bool_ isNpcDSCreate = false;
+    _Npc_* npcD = 0;
+    _Npc_* npcDS = 0;
+    _Army_* armyAS = 0;
+    _Army_* armyDS = 0;
 
     _bool_ isSavedGold = false;
     _int_ goldAttacker = 0;
@@ -142,106 +141,106 @@ int __stdcall Y_ReplayBattle(HiHook* hook, _AdvMgr_* advMng, _dword_ MixedPos, _
         goldDefender = o_GameMgr->players[OwnerD].resourses.gold;
     }
 
-	if (HrA) {
+    if (HrA) {
         o_AdvMgr->HeroActive_DeMobilize();
-		HrAS = (_Hero_*)o_New(1170);
-		MemCopy(HrAS, HrA, 1170);
-		npcA = GetNpc(HrA->id);                       
-		if (npcA->on == 1 && npcA->alive != 1 ) {
-			npcAS = (_Npc_*)o_New(296);
-			MemCopy(npcAS, npcA, 296);             
-			isNpcASCreate = true;
-		}
-	} else { 
-		armyAS = (_Army_*)o_New(56);
-		MemCopy(armyAS, MArrA, 56);
-	}
+        HrAS = (_Hero_*)o_New(1170);
+        MemCopy(HrAS, HrA, 1170);
+        npcA = GetNpc(HrA->id);
+        if (npcA->on == 1 && npcA->alive != 1 ) {
+            npcAS = (_Npc_*)o_New(296);
+            MemCopy(npcAS, npcA, 296);
+            isNpcASCreate = true;
+        }
+    } else { 
+        armyAS = (_Army_*)o_New(56);
+        MemCopy(armyAS, MArrA, 56);
+    }
 
-	if (HrD) {
-		HrDS = (_Hero_*)o_New(1170);
-		MemCopy(HrDS, HrD, 1170);                    
-		npcD = GetNpc(HrD->id);        
-		if (npcD->on == 1 && npcD->alive != 1) {
-			npcDS = (_Npc_*)o_New(296);
-			MemCopy(npcDS, npcD, 296);             
-			isNpcDSCreate = true;
-		}
-	} else { 
-		armyDS = (_Army_*)o_New(56);
-		MemCopy(armyDS, MArrD, 56);
-	}
+    if (HrD) {
+        HrDS = (_Hero_*)o_New(1170);
+        MemCopy(HrDS, HrD, 1170);
+        npcD = GetNpc(HrD->id);
+        if (npcD->on == 1 && npcD->alive != 1) {
+            npcDS = (_Npc_*)o_New(296);
+            MemCopy(npcDS, npcD, 296);
+            isNpcDSCreate = true;
+        }
+    } else { 
+        armyDS = (_Army_*)o_New(56);
+        MemCopy(armyDS, MArrD, 56);
+    }
 
-	// начало цикла повтора битвы
-	do {
-		if (HrA) {
-			MemCopy(HrA, HrAS, 1170);
-			if ( isNpcASCreate ) { 
-				if ( npcAS->on == 1 && npcAS->alive != 1 ) {
-					MemCopy(npcA, npcAS, 296);
-				}
-			}
-		} else {
-			MemCopy(MArrA, armyAS, 56); 
-		}
+    // начало цикла повтора битвы
+    do {
+        if (HrA) {
+            MemCopy(HrA, HrAS, 1170);
+            if ( isNpcASCreate ) { 
+                if ( npcAS->on == 1 && npcAS->alive != 1 ) {
+                    MemCopy(npcA, npcAS, 296);
+                }
+            }
+        } else {
+            MemCopy(MArrA, armyAS, 56);
+        }
 
-		if (HrD) {
-			MemCopy(HrD, HrDS, 1170);
-			if ( isNpcDSCreate ) {
-				if ( npcDS->on == 1 && npcDS->alive != 1 ) {
-					MemCopy(npcD, npcDS, 296);
-				}
-			}
-		} else {
-			MemCopy(MArrD, armyDS, 56);
-		}
+        if (HrD) {
+            MemCopy(HrD, HrDS, 1170);
+            if ( isNpcDSCreate ) {
+                if ( npcDS->on == 1 && npcDS->alive != 1 ) {
+                    MemCopy(npcD, npcDS, 296);
+                }
+            }
+        } else {
+            MemCopy(MArrD, armyDS, 56);
+        }
 
-		if (isNeedReplay) {	//благодоря этой проверке пропускаем первый вызов битвы
+        if (isNeedReplay) { //благодоря этой проверке пропускаем первый вызов битвы
             CALL_0(void, __cdecl, 0x75ACDD); // void __cdecl CheckForCompleteAI()
 
-			// фикс призывов от опыта стеков (вызов существ в 1м раунде битвы)
-			o_BattleMgr->round = 0; 
-			BACall_Day = -1;
-			BACall_Turn = -1; 
+            // фикс призывов от опыта стеков (вызов существ в 1м раунде битвы)
+            o_BattleMgr->round = 0;
+            BACall_Day = -1;
+            BACall_Turn = -1;
 
-			o_QuickBattle = 0;						
-			hdv(_bool_, "HD.QuickCombat") = 0;
+            o_QuickBattle = 0;
+            hdv(_bool_, "HD.QuickCombat") = 0;
 
             if (isSavedGold) {
                 o_GameMgr->players[HrA->owner_id].resourses.gold = goldAttacker;
                 o_GameMgr->players[OwnerD].resourses.gold = goldDefender;
             }
-			
-			FireEvent("OnBattleReplay", NULL, 0);
-		}
+            
+            FireEvent("OnBattleReplay", NULL, 0);
+        }
 
-		// нужен, если выходим в главное меню 
+        // нужен, если выходим в главное меню 
         // через кнопку HD в диалоге настроек битвы
         isNeedReplay = false;
 
-		// непосредственный вызов битвы
-		ret = CALL_11(int, __thiscall, hook->GetDefaultFunc(), advMng, MixedPos, HrA, MArrA, OwnerD, townD, HrD, MArrD, Pv3, Pv2, Pv1);
+        // непосредственный вызов битвы
+        ret = CALL_11(int, __thiscall, hook->GetDefaultFunc(), advMng, MixedPos, HrA, MArrA, OwnerD, townD, HrD, MArrD, Pv3, Pv2, Pv1);
 
-		if ( isNeedReplay ) {
-			FireEvent("OnBeforeBattleReplay", NULL, 0);
-		}
-	} while ( isNeedReplay );
+        if ( isNeedReplay ) {
+            FireEvent("OnBeforeBattleReplay", NULL, 0);
+        }
+    } while ( isNeedReplay );
 
-	// очищаем память
-	if (HrAS) {	o_Delete(HrAS); }
-	if (HrDS) {	o_Delete(HrDS);	}
+    // очищаем память
+    if (HrAS) { o_Delete(HrAS); }
+    if (HrDS) { o_Delete(HrDS); }
 
-	if (npcAS) { o_Delete(npcAS); }
-	if (npcDS) { o_Delete(npcDS); }
+    if (npcAS) { o_Delete(npcAS); }
+    if (npcDS) { o_Delete(npcDS); }
 
-	if (armyAS) { o_Delete(armyAS); }
-	if (armyDS) { o_Delete(armyDS);	}
+    if (armyAS) { o_Delete(armyAS); }
+    if (armyDS) { o_Delete(armyDS); }
 
-	o_QuickBattle = saveParamQB;
-	hdv(_bool_, "HD.QuickCombat") = saveParamQB; 
+    o_QuickBattle = saveParamQB;
+    hdv(_bool_, "HD.QuickCombat") = saveParamQB;
 
-	ifCanReplay = false;
+    ifCanReplay = false;
 
-	return ret; 
+    return ret;
 }
 
 // Астральный дух возвращает существ только после всех переигровок
@@ -261,20 +260,19 @@ void HooksInit()
     _PI->WriteHiHook(0x4716E0, SPLICE_, EXTENDED_, THISCALL_, Y_Dlg_BattleResults_Proc);
 
     // пропускаем обновление мира сразу после битвы
-    _PI->WriteLoHook(0x4173E2, Y_SkipRedrawAdvMap);  
+    _PI->WriteLoHook(0x4173E2, Y_SkipRedrawAdvMap);
  
     // пропускаем всё сразу после битвы
-    _PI->WriteLoHook(0x4ADFE8, Y_AfterBattle_SkipAll);  
+    _PI->WriteLoHook(0x4ADFE8, Y_AfterBattle_SkipAll);
 
     // пропускаем добавление опыта выигравшему
-    _PI->WriteLoHook(0x477254, Y_SkipAddExp);         
-
+    _PI->WriteLoHook(0x477254, Y_SkipAddExp);
 
     // убираем воговский вызов CALL_BATTLE (0x4AD160)
-    _PI->WriteHexPatch(0x75AEB0, "E8 AB22D5FF 90 90");	    
+    _PI->WriteHexPatch(0x75AEB0, "E8 AB22D5FF 90 90");
     // вызываем CALL_BATTLE сами столько раз, сколько нужно
-    // попутно сохраняем структуры нейтралов, героев и командиров   
-    _PI->WriteHiHook(0x75AEB0, CALL_, SAFE_, THISCALL_, Y_ReplayBattle);				
+    // попутно сохраняем структуры нейтралов, героев и командиров
+    _PI->WriteHiHook(0x75AEB0, CALL_, SAFE_, THISCALL_, Y_ReplayBattle);
 
     // Есть баг: Астральный дух возвращает существ перед первой переигровкой
     // С этим хуком Астральный дух возвращает существ только после всех переигровок
@@ -296,10 +294,10 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     case DLL_PROCESS_ATTACH:
         if (!plugin_On)
         {
-            plugin_On = 1;    
+            plugin_On = 1;
 
             _P = GetPatcher();
-            _PI = _P->CreateInstance(PLUGIN_NAME); 
+            _PI = _P->CreateInstance(PLUGIN_NAME);
 
             // подтягиваем ERA
             ConnectEra();
